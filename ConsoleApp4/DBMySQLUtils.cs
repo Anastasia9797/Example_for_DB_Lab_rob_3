6faac649e227612717e3e48823f92aacccc510ab
Anastasia9797 <n6609400@gmail.com>
2024-05-26 14:22:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/DBMySQLUtils.cs
@@ -0,0 +1,17 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace ConsoleApp4
+{
+    class DBMySQLUtils
+    {
+        public static MySqlConnection
+            GetDBConnection(string host, int port, string database, string username, string password)
+        {
+            String connString = "Server = " + host + ";Database = " + database + 
+                ";port = " + port + ";User id = " + username + ";password = " + password;
+            MySqlConnection conn = new MySqlConnection(connString);
+            return conn;
+        }
+
+    }
+}
---
+++ ConsoleApp4/DBUtils.cs
@@ -0,0 +1,17 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace ConsoleApp4
+{
+    class DBUtils
+    {
+        public static MySqlConnection GetDBConnection() 
+        {
+            string host = "localhost";
+            int port = 3306;
+            string database = "world";
+            string username = "monty";
+            string password = "some_password1";
+            return DBMySQLUtils.GetDBConnection(host, port, database, username, password);
+        }
+    }
+}
